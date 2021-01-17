e624eb138a2936fb652a05d24d61f1baa75356bf
Stas <staszhingel@icloud.com>
2021-01-17 07:43:17 +0300

    Initial Commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Стас Жингель on 17.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
